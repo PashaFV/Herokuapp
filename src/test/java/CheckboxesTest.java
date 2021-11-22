dabd5fc15a29e454ca98a46d3c099a69c450db5c
PashaFv <pashafv4@gmail.com>
2021-11-22 23:49:29 +0300

    add Dropdown test.

--- src/test/java/CheckboxesTest.java
+++ src/test/java/CheckboxesTest.java
@@ -2,6 +2,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static org.testng.Assert.*;
 
 public class CheckboxesTest extends BaseTest {
@@ -9,17 +11,14 @@ public class CheckboxesTest extends BaseTest {
     @Test
     public void CheckboxesCheck() {
         driver.get("http://the-internet.herokuapp.com/checkboxes");
-        WebElement firstCheckbox = driver.findElement(By.xpath("//input[@type='checkbox'][1]"));
-        assertFalse(firstCheckbox.isSelected(), "First checkbox is marked");
+        List<WebElement> checkboxes = driver.findElements(By.cssSelector("[type = checkbox]"));
+        assertFalse(checkboxes.get(0).isSelected(), "First checkbox is marked");
+        checkboxes.get(0).click();
+        assertTrue(checkboxes.get(0).isSelected(), "First checkbox not marked");
 
-        firstCheckbox.click();
-        assertTrue(firstCheckbox.isSelected(), "First checkbox not marked");
-
-        WebElement secondCheckbox = driver.findElement(By.xpath("//input[@type='checkbox'][2]"));
-        assertTrue(secondCheckbox.isSelected(), "Second checkbox not marked");
-
-        secondCheckbox.click();
-        assertFalse(secondCheckbox.isSelected(), "Second checkbox is marked");
+        assertTrue(checkboxes.get(1).isSelected(), "Second checkbox not marked");
+        checkboxes.get(1).click();
+        assertFalse(checkboxes.get(1).isSelected(), "Second checkbox is marked");
 
     }
 
